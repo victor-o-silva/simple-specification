f3dc4f80c7ad9eceab8f8ec31bfad21e85cb82b3
Victor Oliveira da Silva <victor_o_silva@hotmail.com>
2019-09-29 22:43:31 -0300

    🔖 Release version 0.2
    [skip ci]

--- setup.py
+++ setup.py
@@ -3,10 +3,12 @@ from setuptools import setup
 with open('README.rst', 'r') as readme_file:
     readme_txt = readme_file.read()
 
+VERSION = '0.2'
+
 setup(
     name='simple-specification',
     packages=['simple_specification'],
-    version='0.1',
+    version=VERSION,
     description='A simple pythonic implementation of the Specification Pattern for Python 3.7 or higher.',
     long_description=readme_txt,
     long_description_content_type='text/x-rst',
@@ -17,5 +19,5 @@ setup(
         'Programming Language :: Python :: 3.7',
     ],
     url='https://github.com/victor-o-silva/simple-specification',
-    download_url='https://github.com/victor-o-silva/simple-specification/tarball/0.1'
+    download_url=f'https://github.com/victor-o-silva/simple-specification/tarball/{VERSION}'
 )
